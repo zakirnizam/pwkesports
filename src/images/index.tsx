045803686fd53dda847daf51f114215715161f11
Zakir Nizam <mohammadzakirnizam@gmail.com>
2025-08-21 07:22:57 +0530

    Updated Images in Ranking

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -42,6 +42,46 @@ import AcchuzImg from './Acchooz.jpg';
 import NizamImg from './Nizam.jpg';
 import SreenathImg from './Sreenath.png';
 import SyamImg from './Syam.jpg';
+import BasimImg from './Basim.jpg';
+import AbrarImg from './Abrar.jpg';
+import AfuveImg from './Afuve.jpg';
+import ZainImg from './Zain.jpg';
+import AdhilImg from './Adhil.jpg';
+import GhostImg from './Ghost.jpg';
+import NaveenImg from './Naveen.jpg';
+import AflahImg from './Aflah.jpg';
+import PranavImg from './Pranav PR.jpg';
+import GokulJsImg from './Gokul GS.jpg';
+import ShamalImg from './Shamal.jpg'
+import RamImg from './Ram.jpg'
+import AmeenImg from './Ameen (1).jpg'
+import AkashImg from './Akash.jpg'
+import ArjuImg from './Arju.jpg'
+import AlfinImg from './Alfin.jpg'
+import AfnasImg from './Afnas.jpg'
+import BipinImg from './Bipin.jpg'
+import KaushikImg from './Kaushik.jpg'
+import ShabithImg from './Shabith.jpg'
+import MBimg from './MB.jpg'
+import SachinImg from './Sachin.jpg'
+import HafezImg from './Hafez.jpg'
+import BilalImg from './Bilal.jpg'
+import FazilImg from './Fazil.jpg'
+import MishalImg from './Mishal.jpg'
+import SabinImg from './Sabin.jpg'
+import AslamImg from './Aslam.jpg'
+import DilshadImg from './Dilshad.jpg'
+import VyshnavImg from './Vyshav.jpg'
+import RockeyImg from './Rockey.jpg'
+import ShijinImg from './Shijin.jpg'
+import SathwinkImg from './Sathwik.jpg'
+import SurajImg from './Suraj.jpg'
+import NevinImg from './Nevin.jpg'
+import AlwinImg from './Alwin.jpg'
+
+
+
+//News Images
 import DistWinnersImg from './DistWinners.png';
 import DistRunnersImg from './DistRunners.png';
 import DistThirdImg from './DistThird.png';
@@ -91,6 +131,49 @@ export const Acchuz = AcchuzImg;
 export const Nizam = NizamImg;
 export const Sreenath = SreenathImg;
 export const Syam = SyamImg;
+export const Basim = BasimImg;
+export const Abrar = AbrarImg;
+export const Afuve = AfuveImg;
+export const Zain = ZainImg;
+export const Adhilvk = AdhilImg;
+export const Ghost = GhostImg;
+export const Naveen = NaveenImg;
+export const Aflah = AflahImg;
+export const Pranavpr = PranavImg;
+export const GokulJs = GokulJsImg;
+export const Shamal = ShamalImg;
+export const Ram = RamImg;
+export const Akash = AkashImg;
+export const Ameen = AmeenImg;
+export const Arju = ArjuImg;
+export const Afnas = AfnasImg;
+export const Bipin = BipinImg;
+export const Kaushik = KaushikImg;
+export const Shabith = ShabithImg;
+export const MB = MBimg;
+export const Alfin = AlfinImg;
+export const Sachin = SachinImg;
+export const Hafez = HafezImg;
+export const Fazil = FazilImg;
+export const Bilal = BilalImg;
+export const Mishal = MishalImg;
+export const Sabin = SabinImg;
+export const Aslam = AslamImg;
+export const Dilshad = DilshadImg;
+export const Rockey = RockeyImg;
+export const Vyshnav = VyshnavImg;
+export const Shijin = ShijinImg;
+export const Sathwik = SathwinkImg;
+export const Suraj = SurajImg;
+export const Nevin = NevinImg;
+export const Alwin = AlwinImg;
+
+
+
+
+
+
+// NEWS IMAGES
 export const DistWinners = DistWinnersImg;
 export const DistRunners = DistRunnersImg;
 export const DistThird = DistThirdImg;
